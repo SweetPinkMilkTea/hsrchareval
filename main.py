a1521b81cb917b8282107023fc5ac91029f06244
SweetPinkMilkTea <supermario3107@gmail.com>
2025-04-03 11:15:47 +0200

    Score System Change

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 import re
 
+def attributeScore(key, metric, target):
+    # Setup of lower bound (Score 0)
+    lower = target / 2
+    if key == "spd":
+        lower = (95 + target) / 2
+    if key == "energy regen":
+        lower = (100 + target) / 2
+    # Score calculation
+    if metric < target:
+        # Get score for non fulfilled target
+        score = (metric - lower) / (target - lower) * 100000
+    else:
+        # Calculated bonus if score exceeds target
+        diff = metric - target
+        score = 100000
+        if key in ["hp","atk","def"]:
+            score += int(diff / 10)
+        if key in ["crit rate"]:
+            score += int(diff)
+        if key in ["crit dmg","break effect","effect hit"]:
+            score += int(diff / 2)
+        if key in ["energy regen", "spd"]:
+            score += int(diff * 2)
+    if score < 0:
+        score = 0
+    return score
+
 
 try:
     import json, time, os, traceback, requests
@@ -155,9 +182,7 @@ try:
                             ratio = 0
                         if ratio > 1:
                             ratio = 1
-                        score = 100000*ratio
-                        if ratio == 1:
-                            score += abs(value2 - value1)
+                        score = attributeScore(i, value1, value2)
                         allscore.append(score)
                         allratio.append(ratio)
                 score = int((sum(allscore) + min(allscore)*5)/(len(allscore)+5))
@@ -205,9 +230,7 @@ try:
                         ratio = 0
                     if ratio > 1:
                         ratio = 1
-                    score = 100000*ratio
-                    if ratio == 1:
-                        score += abs(value2 - value1)
+                    score = attributeScore(i, value1, value2)
                     allscore.append(score)
                     allratio.append(ratio)
                     xvalue1 = f"{value1:,}"
@@ -277,7 +300,7 @@ try:
                                     ratio = 0
                                 if ratio > 1:
                                     ratio = 1
-                                score = 100000*ratio
+                                score = attributeScore(i, value1, value2)
                                 if ratio == 1:
                                     score += abs(value2 - value1)
                                 allscore.append(score)
@@ -621,9 +644,7 @@ try:
                     ratio = 0
                 if ratio > 1:
                     ratio = 1
-                score = 100000*ratio
-                if ratio == 1:
-                    score += abs(value2 - value1)
+                score = attributeScore(i, value1, value2)
                 allscore.append(score)
                 allratio.append(ratio)
                 xvalue1 = f"{value1:,}"
